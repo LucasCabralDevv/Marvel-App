2f138d1274287f9a618dab8ad759dfed7108c8b9
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-05-27 09:53:29 -0300

    Single substituido por viewModel dentro do modulo.

--- app/src/main/java/com/lucascabral/marvelsuperheroes/presenter/di/ViewModelModule.kt
+++ app/src/main/java/com/lucascabral/marvelsuperheroes/presenter/di/ViewModelModule.kt
@@ -1,9 +1,10 @@
 package com.lucascabral.marvelsuperheroes.presenter.di
 
 import com.lucascabral.marvelsuperheroes.presenter.viewmodel.AllCharactersViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
 
-    single { AllCharactersViewModel(get()) }
+    viewModel { AllCharactersViewModel(get()) }
 }
