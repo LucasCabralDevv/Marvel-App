527fc8fa5db0f443cb6ef0f9d6ffc60d91809e1e
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-05-28 10:25:28 -0300

    Testes instrumentados adicionados.

---
+++ app/src/androidTest/java/com/lucascabral/marvelsuperheroes/presenter/view/ActivityTestSuite.kt
@@ -0,0 +1,11 @@
+package com.lucascabral.marvelsuperheroes.presenter.view
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    AllCharactersActivityTest::class,
+    CharacterDetailsActivityTest::class
+)
+class ActivityTestSuite
