0d330a73fd9fdf34c26846be479d4e760f289d41
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-10-29 18:38:37 -0300

    RewardedVideoAd exibido ao clicar em um vídeo.

--- app/src/main/java/com/lucascabral/marvelsuperheroes/presenter/view/MarvelYoutubeActivity.kt
+++ app/src/main/java/com/lucascabral/marvelsuperheroes/presenter/view/MarvelYoutubeActivity.kt
@@ -3,13 +3,15 @@ package com.lucascabral.marvelsuperheroes.presenter.view
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.InterstitialAd
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.reward.RewardItem
+import com.google.android.gms.ads.reward.RewardedVideoAd
+import com.google.android.gms.ads.reward.RewardedVideoAdListener
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.lucascabral.marvelsuperheroes.R
 import com.lucascabral.marvelsuperheroes.databinding.ActivityMarvelYoutubeBinding
@@ -22,6 +24,7 @@ class MarvelYoutubeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMarvelYoutubeBinding
     private lateinit var interstitialAd: InterstitialAd
+    private lateinit var rewardedVideoAd: RewardedVideoAd
     private val viewModelYoutube: MarvelYoutubeViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +33,7 @@ class MarvelYoutubeActivity : AppCompatActivity() {
         setContentView(binding.root)
         setupToolbar()
 
-        initMobileAds()
+        initInterstitialAd()
 
         viewModelYoutube.videos.observe(this, { videosList ->
             if (videosList.isEmpty()) {
@@ -73,7 +76,7 @@ class MarvelYoutubeActivity : AppCompatActivity() {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@MarvelYoutubeActivity)
             adapter = YoutubeAdapter(videos) { video ->
-                initInterstitialAd()
+                initRewardedVideoAd()
                 val intent = YoutubePlayerActivity.getStartIntent(
                     this@MarvelYoutubeActivity,
                     video.id.videoId
@@ -99,6 +102,48 @@ class MarvelYoutubeActivity : AppCompatActivity() {
                 override fun onAdClosed() {
                     Log.d("InterstitialAd_Closed", "Interstitial Closed")
                 }
+            }
+        }
+    }
+
+    private fun initRewardedVideoAd() {
+        rewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this)
+        rewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917", AdRequest.Builder().build())
+        rewardedVideoAd.rewardedVideoAdListener = object : RewardedVideoAdListener {
+            override fun onRewardedVideoAdLoaded() {
+                if (rewardedVideoAd.isLoaded) {
+                    rewardedVideoAd.show()
+                } else {
+                    Log.e("Rewarded_Error", "Error Interstitial Impl")
+                }
+            }
+
+            override fun onRewardedVideoAdOpened() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onRewardedVideoStarted() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onRewardedVideoAdClosed() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onRewarded(p0: RewardItem?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onRewardedVideoAdLeftApplication() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onRewardedVideoAdFailedToLoad(p0: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onRewardedVideoCompleted() {
+                TODO("Not yet implemented")
             }
         }
     }
